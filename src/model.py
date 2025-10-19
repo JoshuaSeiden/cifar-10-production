1631e772508ec5ea093f32616e47b0d53fbad5ac
Joshua Seiden <joshseiden@verizon.net>
2025-10-19 00:09:07 -0400

    defined get_resnet_50_model in model.py

---
+++ src/model.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+import torchvision.models as models
+
+def get_resnet50_model(num_classes=10):
+    model = models.resnet50(weights=models.ResNet50_Weights.IMAGENET1K_V1)
+    for p in model.parameters():
+        p.requires_grad = True
+    in_features = model.fc.in_features
+    model.fc = nn.Linear(in_features, num_classes)
+    return model
