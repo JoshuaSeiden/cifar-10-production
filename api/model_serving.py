308cb72a890b38cb653a26d60c59f4c145e3bf50
Joshua Seiden <joshseiden@verizon.net>
2025-10-19 20:18:23 -0400

    placed resnet18 weights back into hugging face

--- api/model_serving.py
+++ api/model_serving.py
@@ -9,11 +9,9 @@ class ModelServer:
         self.device = device or ("cuda" if torch.cuda.is_available() else "cpu")
         self.model = get_resnet18_model(num_classes=10)
 
-        # Get absolute path to the weights file from project root
-        project_root = Path(__file__).resolve().parents[1]  # one level up from /api
-        model_path = os.path.join(project_root, "models", "cifar_10_resnet18.pth")
 
-        state_dict = torch.load(model_path, map_location=self.device, weights_only=False)
+        url = "https://huggingface.co/jseiden/cifar10-resnet18/resolve/main/cifar_10_resnet18.pth"
+        state_dict = torch.hub.load_state_dict_from_url(url, map_location=self.device)
 
         self.model.load_state_dict(state_dict)
         self.model.to(self.device)
@@ -26,3 +24,4 @@ class ModelServer:
 
 # Singleton instance for the API
 model_server = ModelServer()
+
